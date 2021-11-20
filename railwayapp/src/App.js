bd646da713ca7c54b92305e2a28d61574d0d3b4a
hepnerb <hepnerbryce@gmail.com>
2021-11-19 17:02:17 -0700

    fat push

--- railwayapp/src/App.js
+++ railwayapp/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 import './App.css';
 
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCLrC4PaP-E5F-N2-mCiCgg8lNSzaJvxIs",
+  authDomain: "railway-db.firebaseapp.com",
+  projectId: "railway-db",
+  storageBucket: "railway-db.appspot.com",
+  messagingSenderId: "814197185809",
+  appId: "1:814197185809:web:aa438a4e06480f896d80b0",
+  measurementId: "G-R6YGSPCRMK"
+
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <section>
+        {user ? <Railway /> : <SignIn />}
+      </section>
     </div>
   );
 }
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.signInWithGoogle()
+    auth.signInWithPopup(provider)
+  }
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
 
+function Railway() {
+ return auth.currentUser && (
+
+     <button onClick={() => auth.signOut()}>Sign Out</button>
+ )
+}
 export default App;
